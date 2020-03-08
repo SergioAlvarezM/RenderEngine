c078caffb490a30c8672bbdff87801197572181f
Sergio Andrés Alvarez Medina <ser.alvarez1998@gmail.com>
2020-03-08 12:50:49 -0300

    Documented EventHandler class

--- include/EventHandler.h
+++ include/EventHandler.h
@@ -11,8 +11,12 @@
 /**
  * @brief Class to process all the events that occurs in the engine.
  * 
- * Class that process all the event that happens during the excecution of the engine 
- * and take action depending of the events. Support Mouse and Keyboard events.
+ * Process all the event that happens during the excecution of the engine 
+ * and take action depending of the events via the methods defined by the user.
+ * 
+ * Support Mouse and Keyboard events.
+ * 
+ * To work it needs a Window and Camera Objects to be defined via the setters methods.
  */
 class EventHandler
 {
@@ -31,35 +35,84 @@ public:
      */
     void processInput();
 
-    /* Metodo que sirve para actualizar las variables lastFrame y deltaTime del motor de rendering */
+    /**
+     * @brief Update the variables that control the timing of the engine.
+     * 
+     * Update the lastFrame and deltaTime variables.
+     */
     void recalculateTime();
 
-    /* Metodo que obtiene todos los eventos que ocurrieron en la ventana desde la ultima llamada al metodo.
-    * Desde el inicio si nunca fue llamada
+    /**
+    * @brief Call glfw to get the events that happened in the engine.
+    * 
+    * Get the events that happened since the last call of this method. If the method 
+    * has never been called then obtain all the events that happened since the 
+    * beggining of the engine.
     */
     void getEvents();
 
-    //-----------------------
-    /* SETTERS AND GETTERS */
-    // ----------------------
-
-    /* Function must follow the following contract:
-     *      void function(GLFWwindow *window, double xpos, double ypos)*/
+    /**
+     * @brief Set the Mouse Callback object.
+     * 
+     * Set the mouse Callback method to excecute in a Mouse Click event.
+     * The method should follow the following contract: void function(GLFWwindow *window, double xpos, double ypos).
+     * 
+     * @param method Method to execute when a mouse event is encounter.
+     */
     void setMouseCallback(GLFWcursorposfun method);
 
+    /**
+     * @brief Set the Scroll Callback object.
+     * 
+     * Set the Mouse Scroll Event callback function to excecute in a Mouse Scroll 
+     * Event. The method should follow the following contract: void function(GLFWwindow *window, double xscroll, double yscroll).
+     * 
+     * @param method Method to execute when a mouse scroll event is encounter.
+     */
     void setScrollCallback(GLFWscrollfun method);
 
+    /**
+     * @brief Set the Window object.
+     * 
+     * @param window Window object that is being used by the engine.
+     */
     void setWindow(GLFWwindow *window);
 
+    /**
+     * @brief Set the Camera object.
+     * 
+     * @param camera Camera object that is being used by the engine.
+     */
     void setCamera(Camera *camera);
 
+    /**
+     * @brief Get the Last Frame variable.
+     * 
+     * Get the time when the last frame was executed.
+     * 
+     * @return float Time when the last frame was executed.
+     */
     float getLastFrame() const;
 
+    /**
+     * @brief Get the Delta Time variable.
+     * 
+     * Get the time between the last frame and the current frame.
+     * 
+     * @return float Time between last frame and current frame.
+     */
     float getDeltaTime() const;
 
-    /* CONSTRUCTORS AND DESTRUCTORS */
+    /**
+     * @brief Construct a new Event Handler object
+     * 
+     */
     EventHandler();
 
+    /**
+     * @brief Destroy the Event Handler object
+     * 
+     */
     ~EventHandler();
 };
 
